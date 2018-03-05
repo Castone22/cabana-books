8b137c0a71a93eb0a990c34f759fc14a423d2d14
Lucas Ridge <lzridge.04@gmail.com>
2018-03-05 13:02:32 -0500

    add book listing and styles

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,24 @@
 class BooksController < ApplicationController
-    def new
-    end
+  def index
+    @books = Book.all
+  end
 
-    def create
-        render plain: params[:book].inspect
+  def show
+    @book = Book.find(params[:id])  
+  end
+
+  def new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    @book.save
+    redirect_to @book
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :summary)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resource :books
+  resources :books
 
   root 'welcome#index'
 
