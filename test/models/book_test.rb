adcab61351e35090a48fadc4dda83863f3378a29
Lucas Ridge <lzridge.04@gmail.com>
2018-03-07 11:30:04 -0500

    defining a unit test for books to make sure google doesn't let us save books

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'books are immutable as they are pulled from google books api' do
+    book = Book.new
+    begin
+      book.save
+    rescue ActiveResource::ResourceNotFound => e
+      exception = e
+    ensure
+      assert exception.class.eql? ActiveResource::ResourceNotFound #because nil is still NilClass we can trust this.
+    end
+  end
+
 end
