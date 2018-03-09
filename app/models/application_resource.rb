ab587cf2199e596094ac04a2aadf4ab03730633f
Lucas Ridge <lzridge.04@gmail.com>
2018-03-09 10:36:09 -0500

    providing a way on ActiveResource::Base to make nilsafe calls since google books won't provide nulled fields.

--- app/models/application_resource.rb
+++ app/models/application_resource.rb
@@ -1,2 +1,11 @@
 class ApplicationResource < ActiveResource::Base
 end
+
+class ActiveResource::Base
+  # Safefetch
+  # Attempts to perform a nilsafe fetch of an attribute from a given active resource record.
+  # @param [symbol] attribute_name
+  def sfetch(attribute_name)
+    self&.attributes&.fetch(attribute_name, nil)
+  end
+end
