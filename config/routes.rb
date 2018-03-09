444f9b128b22d12823e0941ea7504305bda5df2f
Lucas Ridge <lzridge.04@gmail.com>
2018-03-09 09:58:17 -0500

    adding jasmine to routes for testing

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  mount JasmineRails::Engine => "/specs" if defined?(JasmineRails)
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
